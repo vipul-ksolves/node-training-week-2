421bd1ca23337eb876c25de019f0d7b3170a0b9a
“vipul-ksolves” <vipul.patel@ksolves.com>
2022-12-26 17:33:29 +0530

    expres framwork

--- express/practice/index.js
+++ express/practice/index.js
@@ -5,12 +5,28 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", "./views");
 
+//middlewere
+const mylogger = (req, res, next) => {
+  console.log("Logged");
+  next();
+};
+
+const requestTime = function (req, res, next) {
+  req.requestTime = Date.now();
+  next();
+};
+
+app.use(mylogger);
+app.use(requestTime);
+
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  let responseText = "Hello World!<br>";
+  responseText += `<h2>Requested at: ${req.requestTime}</h2>`;
+  res.send(responseText);
 });
 
 app.get("/home", function (req, res) {
-  res.render("home");
+  res.send("This is home page");
 });
 
 app.get("/dynamic", function (req, res) {
@@ -20,4 +36,10 @@ app.get("/dynamic", function (req, res) {
   });
 });
 
-app.listen(8000);
+app.post("/post", (req, res) => {
+  res.render("Data is posted");
+});
+
+app.listen(8000, () => {
+  console.log("Server is runnig on port 8000");
+});

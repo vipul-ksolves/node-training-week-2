4ced16a23c6bba3efe8f54021d0526141ffa1e54
“vipul-ksolves” <vipul.patel@ksolves.com>
2022-12-26 14:57:33 +0530

    pug is implemented

--- express/practice/index.js
+++ express/practice/index.js
@@ -2,8 +2,22 @@ import express from "express";
 
 const app = express();
 
+app.set("view engine", "pug");
+app.set("views", "./views");
+
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+app.get("/home", function (req, res) {
+  res.render("home");
+});
+
+app.get("/dynamic", function (req, res) {
+  res.render("dynamic", {
+    name: "Ksolves Info",
+    url: "http://www.ksolves.com",
+  });
 });
 
 app.listen(8000);

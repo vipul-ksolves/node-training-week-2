1249d09544af8e44285c97c6845cb03cf44e7383
“vipul-ksolves” <vipul.patel@ksolves.com>
2023-01-03 14:38:07 +0530

    blog server setup

---
+++ blog/server.js
@@ -0,0 +1,8 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3333;
+
+app.listen(PORT, () => {
+	console.log(`Server is running at ${PORT}`);
+});
--- express/practice/index.js
+++ express/practice/index.js
@@ -1,45 +1,45 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.set("view engine", "pug");
-app.set("views", "./views");
+app.set('view engine', 'pug');
+app.set('views', './views');
 
 //middlewere
 const mylogger = (req, res, next) => {
-  console.log("Logged");
-  next();
+	console.log('Logged');
+	next();
 };
 
 const requestTime = function (req, res, next) {
-  req.requestTime = Date.now();
-  next();
+	req.requestTime = Date.now();
+	next();
 };
 
 app.use(mylogger);
 app.use(requestTime);
 
-app.get("/", (req, res) => {
-  let responseText = "Hello World!<br>";
-  responseText += `<h2>Requested at: ${req.requestTime}</h2>`;
-  res.send(responseText);
+app.get('/', (req, res) => {
+	let responseText = 'Hello World!<br>';
+	responseText += `<h2>Requested at: ${req.requestTime}</h2>`;
+	res.send(responseText);
 });
 
-app.get("/home", function (req, res) {
-  res.send("This is home page");
+app.get('/home', function (req, res) {
+	res.send('This is home page');
 });
 
-app.get("/dynamic", function (req, res) {
-  res.render("dynamic", {
-    name: "Ksolves Info",
-    url: "http://www.ksolves.com",
-  });
+app.get('/dynamic', function (req, res) {
+	res.render('dynamic', {
+		name: 'Ksolves Info',
+		url: 'http://www.ksolves.com',
+	});
 });
 
-app.post("/post", (req, res) => {
-  res.render("Data is posted");
+app.post('/post', (req, res) => {
+	res.render('Data is posted');
 });
 
 app.listen(8000, () => {
-  console.log("Server is runnig on port 8000");
+	console.log('Server is runnig on port 8000');
 });
